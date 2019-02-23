fe880d3a30e4c931dadec1fd52890b093919be60
Ty13R <tpthibodaux@gmail.com>
2019-02-23 12:33:17 -0600

    Change Text

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button.Click
-        Label1.Text = "Hello World"
+        Label1.Text = "Goodbye Cruel World!"
     End Sub
 End Class
